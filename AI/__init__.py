63961179278fe399942583153b2d626d9c243cfb
Ahmet Furkan Ekinci <93397508+Furkan2001@users.noreply.github.com>
2025-06-02 23:19:37 +0300

    Delete AI/__init__.py

--- AI/__init__.py
+++
@@ -1,7 +0,0 @@
-# AI Resource Prediction paketi
-"""
-Elektrik ve su tüketimlerini tahmin etmek için yapay zeka tabanlı bir API.
-"""
-
-__version__ = "1.0.0"
-__author__ = "AI Resource Prediction Team"
